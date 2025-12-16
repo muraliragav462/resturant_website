09bd700f2efae3374c7564018703079a6546972d
muraliragav462 <muralidharan633@gmail.com>
2025-12-16 21:42:07 +0530

    Delete config_paytm.php

--- config_paytm.php
+++
@@ -1,7 +0,0 @@
-   <php
-  define("PAYTM_ENVIROMENT","TEST");
-define("PAYTM_MERCHANT_MID","DIY1238681755550167");
-define("PAYTM_MERCHANT_KEY","bKMfNxPPf_QdZppa");
-define("PAYTM_TXN_URL","https://securegw-stage.paytm.in/order/process");
-define("PAYTM_CALLBACK_URL","http://localhost/paytm/callback.php");
-?>
